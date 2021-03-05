a6771368f1dd28f6dd8f3a0bf71c5b6324914ad7
zhudan <shidan.zhu@dmall.com>
2021-03-05 15:24:06 +0800

    参数

--- batch_exec.js
+++ batch_exec.js
@@ -4,17 +4,20 @@ var fs = require("fs");
 var exec = require('child_process');
 
 var arguments = process.argv.splice(2)
-console.log("说明: 第一个参数配置文件地址，第二个运行js路径, 第三个为并发数量(默认5)，第四个参数为执行时长(s, 默认10s)，第五个可以指定为now表示立即执行")
+console.log("说明: 第一个参数配置文件地址，第二个运行js文件, 第三个为并发数量(默认5)，第四个参数为执行时长(s, 默认10s)，第五个可以指定为now表示立即执行")
 if(arguments.length < 2 ){
     console.log("参数不正确，第一个参数配置文件地址，第二个运行js路径必填")
     return;
 }
-console.log("参数:" + arguments)
+console.log("原参数:" + arguments)
+
 var configFile = arguments[0]
 var taskFile = arguments[1]
-var number = arguments[2] || 5;
+var number = isNaN(arguments[2]) ? 5 : arguments[2];
 var seconds = arguments[3] || 10;
 var now = arguments[2] == 'now' ? 'now': arguments[4];
+
+console.log(`转换后参数: 配置文件: ${configFile}, js文件: ${taskFile}, 并发数量: ${number}, 执行时常: ${seconds}s, 是否立即执行: ${now == 'now'}`)
 
 console.log("配置文件地址:" + configFile);
 if(!fs.existsSync(configFile)){
@@ -56,20 +59,21 @@ var parseConfigAndSetENV = function(){
     console.log(`导入变量: TG_USER_ID = ${process.env.TG_USER_ID}`)
 }
 
+var executeJs = function (index, callback) {
+    console.log("execute [" + (index + 1) + "/" + number + "] " + taskFile);
+    delete require.cache[require.resolve(taskFile)];
+    require(taskFile);
+    setTimeout(callback, 500)
+}
 
 var batchExecute = function(number, func, callback){
     async.timesLimit(9999999, number, func, callback)
 }
 
-var wrapped = async.timeout(batchExecute, seconds * 1000,)
 
 var begin  = function(){
-    wrapped(number, function(index, callback) {
-        console.log("execute [" + (index + 1) + "/" + number + "] " + taskFile);
-        delete require.cache[require.resolve(taskFile)];
-        require(taskFile);
-        setTimeout(callback, 500)
-    }, function () {
+    var wrapped = async.timeout(batchExecute, seconds * 1000,)
+    wrapped(number, executeJs, function () {
         console.log("到达超时时间退出: " + seconds)
         process.exit(0)
     });
