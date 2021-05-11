5803a336d04713d85bd3ca511fc227c1c197892a
zhudan <shidan.zhu@dmall.com>
2021-05-11 11:09:18 +0800

    update

--- batch_exec.js
+++ batch_exec.js
@@ -81,7 +81,7 @@ var executeJs = function (index, callback) {
     console.log("execute [" + (index + 1) + "/" + number + "] " + taskFile);
     delete require.cache[require.resolve(taskFile)];
     require(taskFile);
-    setTimeout(callback, 20)
+    setTimeout(callback, 200)
 }
 
 var batchExecute = function(number, func, callback){
